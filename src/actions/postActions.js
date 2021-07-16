b9e7d5594e23906ebae7ffc34f36623672f5001d
Gus <gsc13@tuta.io>
2021-07-16 13:19:28 -0300

    Add support to Redux Dev tools

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -4,6 +4,7 @@ import { FETCH_POSTS, NEW_POST } from "./types";
 export const fetchPosts = () => (dispatch) => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(res => res.json())
+      // the connected component can dispatch actions by itself, no need for store.dispatch!
       .then(posts => dispatch({
         type: FETCH_POSTS,
         payload: posts,
